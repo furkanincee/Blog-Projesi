446dda71233df60f31e264daf2e88e347e8137a8
Furkan İnce <76855467+furkanincee@users.noreply.github.com>
2022-06-08 13:58:56 +0300

    Repositoryler Oluşturuldu

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    internal interface IBlogDal
+    {
+        List<Blog> ListAllBlogs();
+        void AddBlog(Blog blog);
+        void RemoveBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetById(int id);
+       
+    }
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,14 @@ namespace DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=DESKTOP-8CKLUEG\\SQLEXPRESS;database=Blog; integrated security=true;");
+            optionsBuilder.UseSqlServer("server=DESKTOP-MO5HLC4\\SQLEXPRESS;database=Blog; integrated security=true;");
         }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
