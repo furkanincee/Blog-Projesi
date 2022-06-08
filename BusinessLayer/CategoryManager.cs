386e2203512d5e583921d59a402cd13bfd61014f
furkanincee <76855467+furkanincee@users.noreply.github.com>
2022-06-08 19:37:19 +0300

    Tema eklendi

---
+++ BusinessLayer/CategoryManager.cs
@@ -0,0 +1,37 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class CategoryManager : ICategoryServices
+    {
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
